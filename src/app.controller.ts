352205476a3a1de69705ee4f6bcac24646aacea0
Masoud Javadi <masoudjavadi180@gmail.com>
2023-06-24 17:35:02 +0330

    1 - feat (app.controller)  getAllIncomeReports

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param } from '@nestjs/common';
+import { ReportType, data } from './data';
 
-@Controller('income-reports')
+@Controller('report/:type')
 export class AppController {
   @Get()
-  getAllIncomeReports() {
-    return [];
+  getAllIncomeReports(@Param('type') type: string) {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+    return data?.reports?.filter((report) => report.type === reportType);
+  }
+  @Get(':id')
+  getIncomeReportById() {
+    return {};
+  }
+
+  @Post()
+  createReport() {
+    return 'Created';
+  }
+
+  @Put(':id')
+  updateReport() {
+    return 'Updated';
+  }
+
+  @Delete(':id')
+  deleteReport() {
+    return 'Deleted';
   }
 }
